d8f078bf0746df2fd4ee69e00225ad70c896dadc
Trinendra <trinendra.mehria@gmail.com>
2020-12-07 13:03:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react"
+
+class Welcome extends Component{
+    render(){
+        return (
+                <input type="text" placeholder="Your name"></input>
+        )
+    }
+}
+
+export default Welcome;
